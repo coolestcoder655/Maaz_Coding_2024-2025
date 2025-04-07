61f1c1f29211c7dc704572a9e112bf169c4b728c
Maaz K <khokharmaaz@gmail.com>
2025-04-07 11:20:45 -0500

    Add dependencies for 'run' package and related modules
    Introduced necessary dependencies including 'run', 'minimatch', 'brace-expansion', and 'balanced-match' along with their configuration files (e.g., package.json, LICENSE). These additions support functionality for file monitoring and brace-expansion handling.

--- Websites/Shopping_List/scripts/viewShoppingList.js
+++ Websites/Shopping_List/scripts/viewShoppingList.js
@@ -1,18 +1,23 @@
-const viewContainer = document.getElementById("view-container")
-import {products} from  "./customer.js"
+import {products} from "./customer";
 
-function capitalizeFirstLetter(val) {
-    return String(val).charAt(0).toUpperCase() + String(val).slice(1);
+const container = document.getElementById("products-container");
+
+function displayProducts(container) {
+    Object.values(products).forEach(product => {
+        const productDiv = document.createElement("div");
+        productDiv.id = product.id;
+        productDiv.className = "product-container"; // style with CSS
+        productDiv.style.display = "grid";
+        productDiv.style.placeItems = "center";
+
+        productDiv.innerHTML = `
+        <img class="product" src="${product.imageFilePath}" alt="product">
+        <h3>${product.name}</h3>
+        <h4>${product.price}</h4>
+        `;
+
+        container.appendChild(productDiv);
+    });
 }
 
-for (let product of products) {
-    let child = document.createElement("div");
-    child.id = product.name;
-    child.style.display = "grid";
-    child.style.placeItems = "center";
-    child.innerHTML =
-        `<img class='product' src='${product.imageFilePath}' alt='${product.name}/>` +
-        `<h3>${capitalizeFirstLetter(product.name)}</h3>` +
-        `<h4>${product.price}</h4>`;
-    viewContainer.appendChild(child);
-}
+displayProducts(container);
