b497a60dceefc1c0cfbf6e3478695afb8c2a8da8
Maaz K <khokharmaaz@gmail.com>
2025-04-15 16:00:45 -0500

    Add TypeScript settings and implement simple interest calculation function

---
+++ TypeScript/test.ts
@@ -0,0 +1,5 @@
+function calculateSimpleInterest(principle: number, rate: number, time: number): number {
+    return principle * rate * time;
+}
+
+console.log(calculateSimpleInterest(28538.40, 0.0375, 6))
