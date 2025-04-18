e94d1c4c4c3fd2bdb36b4f1020b3374f157509ce
Maaz K <khokharmaaz@gmail.com>
2025-04-17 19:56:25 -0500

    Added Some Features To The Text Editor

--- Projects/Home Projects/Text_Editor/main.py
+++ Projects/Home Projects/Text_Editor/main.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 from tkinter.filedialog import askopenfilename, asksaveasfilename
+from tkinter import simpledialog, messagebox
 
 def openFile(window: tk.Tk, textEdit: tk.Text):
+    global filePath
     filePath = askopenfilename(filetypes=[("Text Files", "*.txt"), ("Python Files", "*.py"), ("TypeScript Files", "*.ts"), ("Javascript Files", "*.js")])
 
     if not filePath:
@@ -26,6 +28,52 @@ def saveFile(window: tk.Tk, textEdit: tk.Text):
 
     window.title(f"Opened File: {filePath}")
 
+def createNewFile(window: tk.Tk, textEdit: tk.Text, filePath):
+    newfileName = simpledialog.askstring("New File Name?", "Please enter the new file name.")
+
+    try:
+        with open(newfileName, "x") as file:
+            file.write("")
+
+    except FileExistsError as f:
+        messagebox.showerror("ERROR: FILE EXISTS", message=f)
+
+def isFileChanged(textEdit: tk.Text):
+    global filePath
+    with open(filePath, "r") as file:
+        original = file.read()
+
+    modified = textEdit.get(1.0, tk.END)
+
+    try:
+        for i, (char1, char2) in enumerate(zip(original, modified)):
+            if char1 != char2:
+                raise Exception
+
+        # Handle extra characters
+        if len(original) < len(modified):
+            for i in range(len(original), len(modified)):
+                raise Exception
+        elif len(original) > len(modified):
+            for i in range(len(modified), len(original)):
+                raise Exception
+
+    except Exception:
+        return True
+    else:
+        return False
+
+def onCloseConfirmation(window: tk.Tk, textEdit: tk.Text):
+    if isFileChanged(textEdit=textEdit):
+        if messagebox.askyesno("Confirm Exit.", "Are you sure you want to exit without saving?"):
+            window.destroy()
+        else:
+            return
+    else:
+        window.destroy()
+    
+
+
 
 def main():
     window = tk.Tk()
@@ -39,6 +87,7 @@ def main():
     frame = tk.Frame(window, relief=tk.RAISED, bd=2)
     saveButton = tk.Button(frame, text="Save", command=lambda: openFile(window=window, textEdit=textEdit))
     openButton = tk.Button(frame, text="Open", command=lambda: openFile(window=window, textEdit=textEdit))
+    newFileButton = tk.Button(frame, text="New File", command=lambda: createNewFile(window=window, textEdit=textEdit))
 
     saveButton.grid(row=0, column=0, padx=5, pady=5, sticky="ew")
     openButton.grid(row=1, column=0, padx=5, pady=5, sticky="ew")
@@ -46,7 +95,10 @@ def main():
 
     window.bind("<Control-s>", func=lambda x: saveFile(window=window, textEdit=textEdit))
     window.bind("<Control-o>", func=lambda x: openFile(window=window, textEdit=textEdit))
+    window.bind("<Control-n>", func=lambda x: createNewFile(window=window, textEdit=textEdit))
 
+
+    window.protocol("WM_DELETE_WINDOW", func=lambda: onCloseConfirmation(window=window, textEdit=textEdit))
 
     window.mainloop()
 
