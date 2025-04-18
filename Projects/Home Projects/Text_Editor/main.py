4bb19087114808aa066b0054575f0c842ff7ac41
Maaz K <khokharmaaz@gmail.com>
2025-04-18 11:56:03 -0500

    More Stuff :3

--- Projects/Home Projects/Text_Editor/main.py
+++ Projects/Home Projects/Text_Editor/main.py
@@ -4,7 +4,7 @@ from tkinter import simpledialog, messagebox
 
 currentOpenFile = None
 
-def openFile(window: tk.Tk, textEdit: tk.Text):
+def openFile(mainWindow: tk.Tk, textEdit: tk.Text):
     filePath = askopenfilename(filetypes=[("Text Files", "*.txt"), ("Python Files", "*.py"), ("TypeScript Files", "*.ts"), ("Javascript Files", "*.js")])
     global currentOpenFile
     currentOpenFile = filePath
@@ -19,9 +19,9 @@ def openFile(window: tk.Tk, textEdit: tk.Text):
         content = file.read()
 
     textEdit.insert(tk.END, content)
-    window.title(f"Opened File: {filePath}")
+    mainWindow.title(f"Opened File: {filePath}")
 
-def saveFile(window: tk.Tk, textEdit: tk.Text):
+def saveFile(mainWindow: tk.Tk, textEdit: tk.Text):
     filePath = asksaveasfilename(filetypes=[("Text Files", "*.txt"), ("Python Files", "*.py"), ("TypeScript Files", "*.ts"), ("Javascript Files", "*.js")])
 
     if not filePath:
@@ -31,9 +31,9 @@ def saveFile(window: tk.Tk, textEdit: tk.Text):
         content = textEdit.get(1.0, tk.END)
         file.write(content)
 
-    window.title(f"Opened File: {filePath}")
+    mainWindow.title(f"Opened File: {filePath}")
 
-def createNewFile(window: tk.Tk, textEdit: tk.Text):
+def createNewFile(mainWindow: tk.Tk, textEdit: tk.Text):
     newfileName = simpledialog.askstring("New File Name?", "Please enter the new file name.")
 
     try:
@@ -68,53 +68,53 @@ def isFileChanged(textEdit: tk.Text):
     else:
         return False
 
-def onCloseConfirmation(window: tk.Tk, textEdit: tk.Text):
+def onCloseConfirmation(mainWindow: tk.Tk, textEdit: tk.Text):
     global currentOpenFile
     if not currentOpenFile is None:
         if isFileChanged(textEdit=textEdit):
             if messagebox.askyesno("Confirm Exit.", "Are you sure you want to exit without saving?"):
                 currentOpenFile = None
-                window.destroy()
+                mainWindow.destroy()
                 return
             else:
                 return
         else:
             currentOpenFile = None
-            window.destroy()
+            mainWindow.destroy()
             return
     else:
-        window.destroy()
+        mainWindow.destroy()
         return
 
 
 
 def main():
-    window = tk.Tk()
-    window.title("Text Editor")
-    window.rowconfigure(0, minsize=400)
-    window.columnconfigure(1, minsize=250)
+    mainWindow = tk.Tk()
+    mainWindow.title("Text Editor")
+    mainWindow.rowconfigure(0, minsize=400)
+    mainWindow.columnconfigure(1, minsize=250)
 
-    textEdit = tk.Text(window, font="Helvetica 18")
+    textEdit = tk.Text(mainWindow, font="Helvetica 18")
     textEdit.grid(row=0, column=1)
 
-    frame = tk.Frame(window, relief=tk.RAISED, bd=2)
-    saveButton = tk.Button(frame, text="Save", command=lambda: saveFile(window=window, textEdit=textEdit))
-    openButton = tk.Button(frame, text="Open", command=lambda: openFile(window=window, textEdit=textEdit))
-    newFileButton = tk.Button(frame, text="New File", command=lambda: createNewFile(window=window, textEdit=textEdit))
+    frame = tk.Frame(mainWindow, relief=tk.RAISED, bd=2)
+    saveButton = tk.Button(frame, text="Save", command=lambda: saveFile(mainWindow=mainWindow, textEdit=textEdit))
+    openButton = tk.Button(frame, text="Open", command=lambda: openFile(mainWindow=mainWindow, textEdit=textEdit))
+    newFileButton = tk.Button(frame, text="New File", command=lambda: createNewFile(mainWindow=mainWindow, textEdit=textEdit))
 
     saveButton.grid(row=0, column=0, padx=5, pady=5, sticky="ew")
     openButton.grid(row=1, column=0, padx=5, pady=5, sticky="ew")
     newFileButton.grid(row=2, column=0, padx=5, pady=5, sticky="ew")
     frame.grid(row=0, column=0, sticky="ns")
 
-    window.bind("<Control-s>", func=lambda x: saveFile(window=window, textEdit=textEdit))
-    window.bind("<Control-o>", func=lambda x: openFile(window=window, textEdit=textEdit))
-    window.bind("<Control-n>", func=lambda x: createNewFile(window=window, textEdit=textEdit))
+    mainWindow.bind("<Control-s>", func=lambda x: saveFile(mainWindow=mainWindow, textEdit=textEdit))
+    mainWindow.bind("<Control-o>", func=lambda x: openFile(mainWindow=mainWindow, textEdit=textEdit))
+    mainWindow.bind("<Control-n>", func=lambda x: createNewFile(mainWindow=mainWindow, textEdit=textEdit))
 
 
-    window.protocol("WM_DELETE_WINDOW", func=lambda: onCloseConfirmation(window=window, textEdit=textEdit))
+    mainWindow.protocol("WM_DELETE_WINDOW", func=lambda: onCloseConfirmation(mainWindow=mainWindow, textEdit=textEdit))
 
-    window.mainloop()
+    mainWindow.mainloop()
 
 
 main()
